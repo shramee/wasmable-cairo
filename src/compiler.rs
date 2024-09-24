4216a00dc29073a411799fbdfb191b8877593c62
Shramee Srivastav <shramee.srivastav@gmail.com>
2024-09-24 14:17:45 -0400

    chore: cairo compiler and runner wip

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,19 +1,24 @@
 use std::{ffi::OsStr, path::Path};
 
 use anyhow::Result;
-use cairo_lang_filesystem::{
-    db::{CrateConfiguration, FilesGroup, FilesGroupEx},
-    ids::{CrateLongId, Directory},
-};
-
 use cairo_lang_compiler::{
-    compile_prepared_db,
+    compile_prepared_db_program,
     db::RootDatabase,
     diagnostics::{get_diagnostics_as_string, DiagnosticsReporter},
     project::ProjectError,
     CompilerConfig,
 };
+use cairo_lang_runner::{RunResultStarknet, SierraCasmRunner, StarknetState};
+use cairo_lang_sierra_generator::replace_ids::DebugReplacer;
+// use cairo_lang_sierra_generator::{db::SierraGenGroup, program_generator::SierraProgramWithDebug};
+
+use cairo_lang_filesystem::{
+    db::{CrateConfiguration, FilesGroup, FilesGroupEx},
+    ids::{CrateId, CrateLongId, Directory},
+};
 use cairo_lang_sierra::program::Program;
+use cairo_lang_starknet::contract::get_contracts_info;
+// use cairo_lang_starknet::contract::get_contracts_info;
 // use cairo_lang_utils::extract_matches;
 
 // /// Compiles a Cairo project with input String.
@@ -106,16 +111,21 @@ use cairo_lang_sierra::program::Program;
 //     }
 // }
 
-pub fn compile_cairo_project(path: &Path, compiler_config: CompilerConfig<'_>) -> Program {
+pub fn compile_cairo_project(
+    path: &Path,
+    compiler_config: CompilerConfig<'_>,
+) -> Result<(Program, CrateId)> {
+    compile_cairo_project_with_db(path, compiler_config, &mut perpare_db()?)
+}
+
+pub fn compile_cairo_project_with_db(
+    path: &Path,
+    compiler_config: CompilerConfig<'_>,
+    mut db: &mut RootDatabase,
+) -> Result<(Program, CrateId)> {
     let bad_path_err = || ProjectError::BadPath {
         path: path.to_string_lossy().to_string(),
     };
-    let mut db = RootDatabase::builder().detect_corelib().build()?;
-    //build a hashmap of corelib
-    let _file_stem = path
-        .file_stem()
-        .and_then(OsStr::to_str)
-        .ok_or_else(bad_path_err)?;
 
     let canonical = path.canonicalize().map_err(|_| bad_path_err())?;
     let file_dir = canonical.parent().ok_or_else(bad_path_err)?;
@@ -131,7 +141,74 @@ pub fn compile_cairo_project(path: &Path, compiler_config: CompilerConfig<'_>) -
     if DiagnosticsReporter::stderr().check(&db) {
         // TODO: Check if this need extra crate ids.
         let err_string = get_diagnostics_as_string(&mut db, &[]);
-        panic!("failed to compile:\n {}", err_string);
+        anyhow::bail!("failed to compile:\n {}", err_string);
     }
-    Ok(compile_prepared_db(&mut db, vec![crate_id], compiler_config)?.program).unwrap()
+    Ok((
+        compile_prepared_db_program(&mut db, vec![crate_id], compiler_config)?,
+        crate_id,
+    ))
+}
+
+fn perpare_db() -> anyhow::Result<RootDatabase> {
+    let mut db_builder = RootDatabase::builder();
+    db_builder.detect_corelib();
+    db_builder.skip_auto_withdraw_gas();
+    Ok(db_builder.build()?)
+}
+
+pub fn run_cairo_project(
+    path: &Path,
+    compiler_config: CompilerConfig<'_>,
+) -> anyhow::Result<RunResultStarknet> {
+    let mut db = perpare_db()?;
+    let (program, crate_id) = compile_cairo_project_with_db(path, compiler_config, &mut db)?;
+
+    let main_crate_ids = vec![crate_id];
+    let replacer = DebugReplacer { db: &db };
+    let contracts_info = get_contracts_info(&db, main_crate_ids, &replacer)?;
+
+    let runner = SierraCasmRunner::new(program.clone(), None, contracts_info, None)
+        .map_err(|err| anyhow::anyhow!(err.to_string()))?;
+
+    let result = runner
+        .run_function_with_starknet_context(
+            runner
+                .find_function("::main")
+                .map_err(|err| anyhow::anyhow!(err.to_string()))?,
+            &[],
+            None,
+            StarknetState::default(),
+        )
+        // .with_context(|| "Failed to run the function.")?;
+        .map_err(|err| anyhow::anyhow!(err.to_string()))?;
+
+    Ok(result)
+}
+
+pub fn run_cairo_program(
+    program: Program,
+    _compiler_config: CompilerConfig<'_>,
+) -> anyhow::Result<RunResultStarknet> {
+    let db = perpare_db()?;
+
+    let main_crate_ids = vec![];
+    let replacer = DebugReplacer { db: &db };
+    let contracts_info = get_contracts_info(&db, main_crate_ids, &replacer)?;
+
+    let runner = SierraCasmRunner::new(program.clone(), None, contracts_info, None)
+        .map_err(|err| anyhow::anyhow!(err.to_string()))?;
+
+    let result = runner
+        .run_function_with_starknet_context(
+            runner
+                .find_function("::main")
+                .map_err(|err| anyhow::anyhow!(err.to_string()))?,
+            &[],
+            None,
+            StarknetState::default(),
+        )
+        // .with_context(|| "Failed to run the function.")?;
+        .map_err(|err| anyhow::anyhow!(err.to_string()))?;
+
+    Ok(result)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::fs;
 use std::path::PathBuf;
 
 use cairo_lang_compiler::CompilerConfig;
+use cairo_lang_sierra::program::Program;
 use clap::{Parser, Subcommand};
-use wasmable_cairo::compiler::compile_cairo_project;
+use wasmable_cairo::compiler::{compile_cairo_project, run_cairo_project};
 
 /// The main CLI application structure
 #[derive(Parser)]
@@ -16,7 +18,9 @@ struct Cli {
 /// Enum to define available subcommands
 #[derive(Subcommand)]
 enum Commands {
-    Build { path: PathBuf },
+    Compile { path: PathBuf },
+    Run { path: PathBuf },
+    RunSierra { path: PathBuf },
 }
 
 fn main() {
@@ -25,14 +29,41 @@ fn main() {
         replace_ids: false,
         ..CompilerConfig::default()
     };
+    let sierra_path = &PathBuf::from("target");
+    fs::create_dir_all(&sierra_path).unwrap();
+    let sierra_path = sierra_path.canonicalize().unwrap().join("sierra.json");
+    // create file if not exists
+    if !sierra_path.exists() {
+        fs::write(&sierra_path, "").unwrap();
+    }
 
     match &cli.command {
-        Commands::Build { path } => {
-            let result = compile_cairo_project(path, compiler_config);
-            match result {
-                Ok(program) => println!("{:?}", serde_json::to_string(&program)),
-                _ => {}
+        Commands::Compile { path } => match compile_cairo_project(path, compiler_config) {
+            Ok((program, _crate_id)) => {
+                let program_json = serde_json::to_string(&program).unwrap();
+                fs::write(&sierra_path, program_json).unwrap();
+                println!("File saved at: {}", sierra_path.as_path().to_str().unwrap());
             }
+            Err(e) => panic!("Error compiling Cairo project: {}", e),
+        },
+        Commands::Run { path } => match run_cairo_project(path, compiler_config) {
+            Ok(result) => {
+                println!("Result: {:#?}", result.value);
+                // let program_json = serde_json::to_string(&program).unwrap();
+                // fs::write(&sierra_path, program_json).unwrap();
+                // println!("File saved at: {}", sierra_path.as_path().to_str().unwrap());
+            }
+            Err(e) => panic!("Error compiling Cairo project: {}", e),
+        },
+        Commands::RunSierra { path } => {
+            match fs::read_to_string(path) {
+                Ok(content) => {
+                    let _program: Program = serde_json::from_str(&content).unwrap();
+                }
+                Err(e) => eprintln!("Error reading file: {}", e),
+            }
+            let (program, _) = compile_cairo_project(path, compiler_config).unwrap();
+            println!("{}", serde_json::to_string(&program).unwrap());
         }
     };
 }
